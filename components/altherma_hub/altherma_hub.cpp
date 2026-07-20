d5631661366d825e69ca81427cbac7005426b824
PesseM <michael.pessers@gmail.com>
2026-07-20 14:47:15 +0200

    Update altherma_hub.cpp
    Fix library header paths for ESPHome 2026.7

--- components/altherma_hub/altherma_hub.cpp
+++ components/altherma_hub/altherma_hub.cpp
@@ -1,7 +1,7 @@
 #include "altherma_hub.h"
 #include "esphome/core/log.h"
 #include "mock_uart.h"
-#include "labeldef.h"
+#include "lib/labeldef.h"
 #include <cstdarg>
 #include <cstdio>
 LabelDef labelDefs[] = {};
